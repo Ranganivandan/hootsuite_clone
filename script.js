5db9fdd3b16ed987d5e1063d3782e4c60e46d832
ranganivandan <vandanrangani21@gmail.com>
2024-01-10 12:06:23 +0530

    update

---
+++ script.js
@@ -0,0 +1,97 @@
+
+
+
+function show_menu()
+{
+document.getElementById('overeffect').style.visibility="visible";
+document.getElementById('parahead').innerHTML="Schedule across all your networks in a few taps";
+document.getElementById('para').innerHTML="Naptime awaits. With Hootsuite, you can design, schedule, and publish content for all your networks from one dashboard. Create dazzling posts fast with Canva templates, have AI write your captions and hashtags for you, then get approval with seamless workflows.";
+document.getElementById("overeffect").style.transform = "rotate(180deg)";
+}
+function hide() {
+    
+    document.getElementById('parahead').innerHTML="";
+    document.getElementById('para').innerHTML="";
+  
+}
+function hide2() {
+    document.getElementById('parahead2').innerHTML="";
+    document.getElementById('para2').innerHTML="";
+      
+}
+function hide3() {
+    document.getElementById('parahead3').innerHTML="";
+    document.getElementById('para3').innerHTML="";
+      
+}
+function hide4() {
+    document.getElementById('parahead4').innerHTML="";
+    document.getElementById('para4').innerHTML="";
+      
+}
+function hide5() {
+    document.getElementById('parahead5').innerHTML="";
+    document.getElementById('para5').innerHTML="";
+      
+}
+
+
+
+
+
+function hide_menu() {
+    document.getElementById("overeffect").style.transform = "rotate(360deg)";
+
+}
+function show_menu2()
+{
+
+    
+document.getElementById('overeffect2').style.visibility="visible";
+document.getElementById("overeffect2").style.transform = "rotate(180deg)";
+document.getElementById('parahead2').innerHTML="Schedule across all your networks in a few taps";
+document.getElementById('para2').innerHTML="Naptime awaits. With Hootsuite, you can design, schedule, and publish content for all your networks from one dashboard. Create dazzling posts fast with Canva templates, have AI write your captions and hashtags for you, then get approval with seamless workflows.";
+}
+
+
+function hide_menu2() {
+    document.getElementById("overeffect2").style.transform = "rotate(360deg)";
+}
+
+function show_menu3()
+{
+document.getElementById('overeffect3').style.visibility="visible";
+
+document.getElementById("overeffect3").style.transform = "rotate(180deg)";
+document.getElementById('parahead3').innerHTML="Schedule across all your networks in a few taps";
+document.getElementById('para3').innerHTML="Naptime awaits. With Hootsuite, you can design, schedule, and publish content for all your networks from one dashboard. Create dazzling posts fast with Canva templates, have AI write your captions and hashtags for you, then get approval with seamless workflows.";
+}
+
+
+function hide_menu3() {
+    document.getElementById("overeffect3").style.transform = "rotate(360deg)";
+}
+
+function show_menu4()
+{
+document.getElementById('overeffect4').style.visibility="visible";
+document.getElementById('parahead4').innerHTML="Schedule across all your networks in a few taps";
+document.getElementById('para4').innerHTML="Naptime awaits. With Hootsuite, you can design, schedule, and publish content for all your networks from one dashboard. Create dazzling posts fast with Canva templates, have AI write your captions and hashtags for you, then get approval with seamless workflows.";
+document.getElementById("overeffect4").style.transform = "rotate(180deg)";
+}
+
+function hide_menu4() {
+    document.getElementById("overeffect4").style.transform = "rotate(360deg)";
+}
+
+function show_menu5()
+{
+document.getElementById('overeffect5').style.visibility="visible";
+document.getElementById('parahead5').innerHTML="Schedule across all your networks in a few taps";
+document.getElementById('para5').innerHTML="Naptime awaits. With Hootsuite, you can design, schedule, and publish content for all your networks from one dashboard. Create dazzling posts fast with Canva templates, have AI write your captions and hashtags for you, then get approval with seamless workflows.";
+document.getElementById("overeffect5").style.transform = "rotate(180deg)";
+}
+
+function hide_menu5() {
+    document.getElementById("overeffect5").style.transform = "rotate(360deg)";
+}
