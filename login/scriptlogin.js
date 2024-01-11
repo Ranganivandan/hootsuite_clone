1bf3be4465d3b09fc8d174ee95165af39358bff8
ranganivandan <vandanrangani21@gmail.com>
2024-01-11 11:35:37 +0530

    update

---
+++ login/scriptlogin.js
@@ -0,0 +1,24 @@
+
+
+function signstore(){
+
+    const userdata = [];
+const input1 = document.getElementById("email");
+const input2 = document.getElementById("pass");
+
+const user = input1.value;
+const pass = input2.value;
+
+userdata.push(user,pass);
+    
+   console.log(userdata);
+}
+
+
+
+
+
+
+
+
+
